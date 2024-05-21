91ffd804f0543caac788ae9559f4a18deca11ea1
kooreung <crisishyun@gmail.com>
2024-05-21 15:13:39 +0900

    게시물 하나 확인하기

--- src/page/board/BoardView.jsx
+++ src/page/board/BoardView.jsx
@@ -1,13 +1,55 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Spinner,
+  Textarea,
+} from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export function BoardView() {
   const { id } = useParams();
+  const [board, setBoard] = useState(null);
+
   useEffect(() => {
-    axios.get(`/api/board/${id}`);
+    axios.get(`/api/board/${id}`).then((res) => setBoard(res.data));
   }, []);
 
-  return <Box></Box>;
+  // 기다리는 동안 로딩 화면
+  if (board === null) {
+    return <Spinner />;
+  }
+
+  return (
+    <Box>
+      <Box>{board.id} 번 게시물</Box>
+      <Box>
+        <FormControl>
+          <FormLabel>제목</FormLabel>
+          <Input value={board.title} readOnly />
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+          <FormLabel>본문</FormLabel>
+          <Textarea value={board.content} readolny />
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+          <FormLabel>작성자</FormLabel>
+          <Input value={board.writer} readOnly />
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+          <FormLabel>작성일시</FormLabel>
+          <Input type={"datetime-local"} value={board.inserted} readonly />
+        </FormControl>
+      </Box>
+    </Box>
+  );
 }
