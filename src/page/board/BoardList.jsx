67e4809a2bf2534664a72222cf5f1356caef917d
kooreung <crisishyun@gmail.com>
2024-05-21 14:39:05 +0900

    BoardList 의 Table 클릭 시 게시물 보기

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -3,9 +3,11 @@ import { Box, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserPen } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get("/api/board/list").then((res) => setBoardList(res.data));
@@ -27,7 +29,12 @@ export function BoardList() {
           </Thead>
           <Tbody>
             {boardList.map((board) => (
-              <Tr key={board.id}>
+              <Tr
+                key={board.id}
+                onClick={() => navigate(`/board/${board.id}`)}
+                cursor={"pointer"}
+                _hover={{ bgColor: "gray.50" }}
+              >
                 <Td>{board.id}</Td>
                 <Td>{board.title}</Td>
                 <Td>{board.writer}</Td>
