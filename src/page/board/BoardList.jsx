efc46e503f6d40ff7527b9071023768e3e07fd86
kooreung <crisishyun@gmail.com>
2024-05-21 14:20:10 +0900

    게시물 목록 보기 화면

---
+++ src/page/board/BoardList.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Box, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserPen } from "@fortawesome/free-solid-svg-icons";
+
+export function BoardList() {
+  const [boardList, setBoardList] = useState([]);
+  return (
+    <Box>
+      <Box fontWeight={"bold"}>게시물 목록</Box>
+      <Box>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>#</Th>
+              <Th>TITLE</Th>
+              <Th>
+                <FontAwesomeIcon icon={faUserPen} />
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {boardList.map((board) => (
+              <Tr key={board.id}>
+                <Td>{board.id}</Td>
+                <Td>{board.title}</Td>
+                <Td>{board.writer}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
+    </Box>
+  );
+}
