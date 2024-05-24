2d740ed84a961f068f6aaa8876fc55c3c4152064
kooreung <crisishyun@gmail.com>
2024-05-24 10:49:47 +0900

    글 삭제 토큰 내용 오류 수정

--- src/page/board/BoardView.jsx
+++ src/page/board/BoardView.jsx
@@ -48,26 +48,28 @@ export function BoardView() {
   }
 
   function handleRemoveClick() {
-    axios.delete(`/api/board/${id}`),
-      { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` } }
-        .then(() => {
-          toast({
-            status: "success",
-            position: "top",
-            description: `${id}번 게시물이 삭제 되었습니다.`,
-          });
-          navigate("/");
-        })
-        .catch(() => {
-          toast({
-            status: "error",
-            position: "top",
-            description: `${id}번 게시물 삭제에 실패하였습니다.`,
-          });
-        })
-        .finally(() => {
-          onClose();
+    axios
+      .delete(`/api/board/${id}`, {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      })
+      .then(() => {
+        toast({
+          status: "success",
+          position: "top",
+          description: `${id}번 게시물이 삭제 되었습니다.`,
         });
+        navigate("/");
+      })
+      .catch(() => {
+        toast({
+          status: "error",
+          position: "top",
+          description: `${id}번 게시물 삭제에 실패하였습니다.`,
+        });
+      })
+      .finally(() => {
+        onClose();
+      });
   }
 
   return (
