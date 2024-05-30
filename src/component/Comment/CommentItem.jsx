3e7d25f88fdf0fb7364d33d86c3142d84f9de44c
kooreung <crisishyun@gmail.com>
2024-05-30 16:34:33 +0900

    코멘트 삭제 모달

--- src/component/Comment/CommentItem.jsx
+++ src/component/Comment/CommentItem.jsx
@@ -1,9 +1,23 @@
-import { Box, Button, Flex, Spacer, useToast } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spacer,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 
 export function CommentItem({ comment, isProcessing, setIsProcessing }) {
+  const { isOpen, onClose, onOpen } = useDisclosure();
   const toast = useToast();
 
   function handleCommentRemoveClick() {
@@ -12,40 +26,56 @@ export function CommentItem({ comment, isProcessing, setIsProcessing }) {
       .delete(`/api/comment/remove`, {
         data: { id: comment.id },
       })
-      .then((res) => {
+      .then(() => {
         toast({
-          status: "success",
+          status: "info",
           position: "top",
           description: "댓글이 삭제되었습니다.",
         });
       })
       .catch((err) => {})
       .finally(() => {
+        onClose();
         setIsProcessing(false);
       });
   }
 
   return (
-    <Box>
-      <Box border={"1px solid black"} my={3}>
-        <Flex>
-          <Box>{comment.nickName}</Box>
-          <Spacer />
-          <Box>{comment.inserted}</Box>
-        </Flex>
-        <Flex>
-          <Box>{comment.comment}</Box>
-          <Box>
+    <Box border={"1px solid black"} my={3}>
+      <Flex>
+        <Box>{comment.nickName}</Box>
+        <Spacer />
+        <Box>{comment.inserted}</Box>
+      </Flex>
+      <Flex>
+        <Box>{comment.comment}</Box>
+        <Box>
+          <Button
+            onClick={onOpen}
+            colorScheme={"orange"}
+            isLoading={isProcessing}
+          >
+            <FontAwesomeIcon icon={faTrashCan} />
+          </Button>
+        </Box>
+      </Flex>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>삭제 확인</ModalHeader>
+          <ModalBody>댓글을 삭제하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>취소</Button>
             <Button
-              colorScheme="red"
               onClick={handleCommentRemoveClick}
+              colorScheme={"red"}
               isLoading={isProcessing}
             >
-              <FontAwesomeIcon icon={faTrashCan} />
+              삭제
             </Button>
-          </Box>
-        </Flex>
-      </Box>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
